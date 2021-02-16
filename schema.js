88edb726e55d9e88ab9eae64910bdf413626fd49
Christopher Kim <chrisms91@gmail.com>
2021-02-15 20:37:47 -0800

    generate initial file structure divided by domain

---
+++ schema.js
@@ -0,0 +1,18 @@
+import {
+  loadFilesSync,
+  makeExecutableSchema,
+  mergeResolvers,
+  mergeTypeDefs,
+} from 'graphql-tools';
+
+const loadedTypeDefs = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
+const loadedResolvers = loadFilesSync(
+  `${__dirname}/**/*.{queries, mutations}.js`
+);
+
+const typeDefs = mergeTypeDefs(loadedTypeDefs);
+const resolvers = mergeResolvers(loadedResolvers);
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+export default schema;
