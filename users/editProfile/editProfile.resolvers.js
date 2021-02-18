1d38ec0408c42e4dd39942cf1a78c58d685b3ca4
Christopher Kim <chrisms91@gmail.com>
2021-02-18 01:45:39 -0800

    add editProfile

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,8 +1,37 @@
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import client from '../../client';
 
 export default {
   Mutation: {
-    editProfile: () => console.log('hi'),
+    editProfile: async (
+      _,
+      { firstName, lastName, userName, email, password: newPassword }
+    ) => {
+      try {
+        let hashedPassword = null;
+        if (newPassword) {
+          hashedPassword = await bcrypt.hash(newPassword, 10);
+        }
+        const updatedUser = await client.user.update({
+          // TODO
+          // add authentication part to get the current user's id
+          where: { id: 1 },
+          data: {
+            firstName,
+            lastName,
+            userName,
+            email,
+            ...(hashedPassword && { password: hashedPassword }), // the spread operator unpacks iterable object
+          },
+        });
+
+        return updatedUser.id
+          ? { ok: true }
+          : { ok: false, error: 'Could not update profile.' };
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 };
