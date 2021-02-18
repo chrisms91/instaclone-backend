6add59d16b612061e976304cf86e34ebaaf163bd
Christopher Kim <chrisms91@gmail.com>
2021-02-17 22:31:54 -0800

    add login with jwt

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import client from '../client';
 
 export default {
@@ -32,6 +33,26 @@ export default {
         return newUser;
       } catch (error) {
         return error;
+      }
+    },
+
+    login: async (_, { userName, password }) => {
+      try {
+        const user = await client.user.findFirst({ where: { userName } });
+        if (!user) {
+          return { ok: false, error: 'User not found.' };
+        }
+
+        const passwordOk = await bcrypt.compare(password, user.password);
+        if (!passwordOk) {
+          return { ok: false, error: 'Incorrect Password.' };
+        }
+
+        // issue a token and send it to the user
+        const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+        return { ok: true, token };
+      } catch (error) {
+        console.log(error);
       }
     },
   },
