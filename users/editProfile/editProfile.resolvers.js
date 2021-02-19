909696e8cc1f7371b5cadfa1fe0d44c9cb9e53d4
Christopher Kim <chrisms91@gmail.com>
2021-02-19 00:13:58 -0800

    add protection to the resolvers

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,38 +1,41 @@
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import client from '../../client';
+import { protectedResolver } from '../users.utils';
+
+const resolverFn = async (
+  _,
+  { firstName, lastName, userName, email, password: newPassword },
+  { loggedInUser, protectResolver }
+) => {
+  try {
+    console.log('currently loggedIn user');
+    console.log(loggedInUser);
+    let hashedPassword = null;
+    if (newPassword) {
+      hashedPassword = await bcrypt.hash(newPassword, 10);
+    }
+    const updatedUser = await client.user.update({
+      where: { id: loggedInUser.id },
+      data: {
+        firstName,
+        lastName,
+        userName,
+        email,
+        ...(hashedPassword && { password: hashedPassword }), // the spread operator unpacks iterable object
+      },
+    });
+
+    return updatedUser.id
+      ? { ok: true }
+      : { ok: false, error: 'Could not update profile.' };
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export default {
   Mutation: {
-    editProfile: async (
-      _,
-      { firstName, lastName, userName, email, password: newPassword },
-      { loggedInUser }
-    ) => {
-      try {
-        console.log('currently loggedIn user');
-        console.log(loggedInUser);
-        let hashedPassword = null;
-        if (newPassword) {
-          hashedPassword = await bcrypt.hash(newPassword, 10);
-        }
-        const updatedUser = await client.user.update({
-          where: { id: loggedInUser.id },
-          data: {
-            firstName,
-            lastName,
-            userName,
-            email,
-            ...(hashedPassword && { password: hashedPassword }), // the spread operator unpacks iterable object
-          },
-        });
-
-        return updatedUser.id
-          ? { ok: true }
-          : { ok: false, error: 'Could not update profile.' };
-      } catch (error) {
-        console.log(error);
-      }
-    },
+    editProfile: protectedResolver(resolverFn),
   },
 };
--- users/users.utils.js
+++ users/users.utils.js
@@ -4,8 +4,10 @@ import client from '../client';
 export const getUser = async (token) => {
   try {
     if (!token) return null;
+
     const { id } = await jwt.verify(token, process.env.SECRET_KEY);
     const user = await client.user.findUnique({ where: { id } });
+
     if (!user) return null;
 
     return user;
@@ -14,3 +16,17 @@ export const getUser = async (token) => {
     return null;
   }
 };
+
+// returning a function that it not called yet which is regular graphql resolver.
+export function protectedResolver(resolver) {
+  return function (root, args, context, info) {
+    if (!context.loggedInUser) {
+      return {
+        ok: false,
+        error: 'Please login to perform this action.',
+      };
+    }
+
+    return resolver(root, args, context, info);
+  };
+}
