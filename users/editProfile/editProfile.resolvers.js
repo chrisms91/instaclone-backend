359270ad035405835f5135880f3f8cf0ed6b867b
Christopher Kim <chrisms91@gmail.com>
2021-02-20 01:49:25 -0800

    change node version

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -7,38 +7,42 @@ import { protectedResolver } from '../users.utils';
 const resolverFn = async (
   _,
   { firstName, lastName, userName, email, password: newPassword, bio, avatar },
-  { loggedInUser, protectResolver }
+  { loggedInUser }
 ) => {
-  try {
+  let avatarUrl = null;
+  if (avatar) {
     const { filename, mimetype, createReadStream } = await avatar;
+    const newFileName = `${loggedInUser.id}-${Date.now()}-${filename}`;
     const readStream = createReadStream();
     const writeStream = fs.createWriteStream(
-      process.cwd() + '/uploads/' + filename
+      process.cwd() + '/uploads/' + newFileName
     );
+    // it will be stored in the cloud on production.
     await readStream.pipe(writeStream);
-
-    let hashedPassword = null;
-    if (newPassword) {
-      hashedPassword = await bcrypt.hash(newPassword, 10);
-    }
-    const updatedUser = await client.user.update({
-      where: { id: loggedInUser.id },
-      data: {
-        firstName,
-        lastName,
-        userName,
-        email,
-        bio,
-        ...(hashedPassword && { password: hashedPassword }), // the spread operator unpacks iterable object
-      },
-    });
-
-    return updatedUser.id
-      ? { ok: true }
-      : { ok: false, error: 'Could not update profile.' };
-  } catch (error) {
-    console.log(error);
+    avatarUrl = `http://localhost:4000/static/${newFileName}`;
   }
+
+  let hashedPassword = null;
+  if (newPassword) {
+    hashedPassword = await bcrypt.hash(newPassword, 10);
+  }
+  const updatedUser = await client.user.update({
+    where: { id: loggedInUser.id },
+    data: {
+      firstName,
+      lastName,
+      userName,
+      email,
+      bio,
+      // check if hashedPassword exists then ... operator unpacks object
+      ...(hashedPassword && { password: hashedPassword }),
+      ...(avatarUrl && { avatar: avatarUrl }),
+    },
+  });
+
+  return updatedUser.id
+    ? { ok: true }
+    : { ok: false, error: 'Could not update profile.' };
 };
 
 export default {
